be78ebfc1018b11afbbbdd62d61fe2a8e585cf7c
kirlozavr <kirill.protchenko@mail.ru>
2023-03-26 16:28:13 +1000

    Начата работа над presenter

--- app/src/main/java/com/example/sendmessageskotlin/presenter/ChatsPresenter.kt
+++ app/src/main/java/com/example/sendmessageskotlin/presenter/ChatsPresenter.kt
@@ -1,4 +1,33 @@
 package com.example.sendmessageskotlin.presenter
 
-class ChatsPresenter {
+import android.content.Context
+import com.example.sendmessageskotlin.common.CallBackHandler
+import com.example.sendmessageskotlin.common.StartActivityCallBack
+import com.example.sendmessageskotlin.entity.ChatsEntity
+import com.example.sendmessageskotlin.mapping.ChatsMapper
+
+class ChatsPresenter
+constructor(context: Context, startActivityCallBack: StartActivityCallBack){
+
+    private val USERNAME_FROM = "usernameFromChats"
+
+    private var isToday = true
+    private val context: Context
+    private lateinit var usernameFrom: String
+    private val mapper = ChatsMapper()
+    private val startActivityCallBack: StartActivityCallBack
+    private val getChatsCallBack = object : CallBackHandler<ChatsEntity>{
+        override fun execute(value: ChatsEntity) {
+
+        }
+    }
+
+    init {
+        this.context = context
+        this.startActivityCallBack = startActivityCallBack
+    }
+
+    fun showChats(usernameFrom: String){
+
+    }
 }
