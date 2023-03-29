636c5090d8040d6b2264b8b9e3d3aef3c8fbf84d
kirlozavr <kirill.protchenko@mail.ru>
2023-03-29 19:40:51 +1000

    Добавил тест для модели

---
+++ app/src/androidTest/java/com/example/sendmessageskotlin/ChatsModelTest.kt
@@ -0,0 +1,24 @@
+﻿package com.example.sendmessageskotlin
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.sendmessageskotlin.common.CallBackHandler
+import com.example.sendmessageskotlin.entity.ChatsEntity
+import com.example.sendmessageskotlin.model.ChatsModel
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ChatsModelTest {
+
+    @Test
+    fun getAllChatsByName(){
+        var chatsModel = ChatsModel()
+        val callBack = object : CallBackHandler<List<ChatsEntity>> {
+            override fun execute(value: List<ChatsEntity>) {
+                Assert.assertTrue(value.isNotEmpty())
+            }
+        }
+        chatsModel.getAllChats(callBack, "папа")
+    }
+}
