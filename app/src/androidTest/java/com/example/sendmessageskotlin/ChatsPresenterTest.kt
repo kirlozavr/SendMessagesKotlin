c0027b9426546290b0ffa2678ed858f6103bf1d1
kirlozavr <kirill.protchenko@mail.ru>
2023-03-30 16:57:15 +1000

    Тест для ChatsPresenter

---
+++ app/src/androidTest/java/com/example/sendmessageskotlin/ChatsPresenterTest.kt
@@ -0,0 +1,43 @@
+﻿package com.example.sendmessageskotlin
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.sendmessageskotlin.adapters.RecyclerViewCallBack
+import com.example.sendmessageskotlin.dto.ChatsDto
+import com.example.sendmessageskotlin.presenter.ChatsPresenter
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ChatsPresenterTest {
+
+    private lateinit var chatsPresenter: ChatsPresenter
+
+    @Before
+    fun startUp(){
+        chatsPresenter = ChatsPresenter()
+    }
+
+    @Test
+    fun showChatsTestIsCorrect(){
+        val recyclerViewCallBack = object : RecyclerViewCallBack<List<ChatsDto>> {
+            override fun get(value: List<ChatsDto>) {
+                Assert.assertTrue(value.isNotEmpty())
+            }
+
+        }
+        chatsPresenter.showChats("папа", recyclerViewCallBack)
+    }
+
+    @Test
+    fun showChatsTestIsNotCorrect(){
+        val recyclerViewCallBack = object : RecyclerViewCallBack<List<ChatsDto>> {
+            override fun get(value: List<ChatsDto>) {
+                Assert.assertTrue(value.isEmpty())
+            }
+
+        }
+        chatsPresenter.showChats("папа1", recyclerViewCallBack)
+    }
+}
