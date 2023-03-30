e6a0e3ae47292c27173b6f6c043ad14423de85d8
kirlozavr <kirill.protchenko@mail.ru>
2023-03-30 16:56:50 +1000

    Добавлена логика работы презентера

--- app/src/main/java/com/example/sendmessageskotlin/presenter/ChatsPresenter.kt
+++ app/src/main/java/com/example/sendmessageskotlin/presenter/ChatsPresenter.kt
@@ -1,33 +1,51 @@
 package com.example.sendmessageskotlin.presenter
 
-import android.content.Context
+import com.example.sendmessageskotlin.adapters.RecyclerViewCallBack
 import com.example.sendmessageskotlin.common.CallBackHandler
-import com.example.sendmessageskotlin.common.StartActivityCallBack
+import com.example.sendmessageskotlin.common.DateFormat
+import com.example.sendmessageskotlin.dto.ChatsDto
 import com.example.sendmessageskotlin.entity.ChatsEntity
 import com.example.sendmessageskotlin.mapping.ChatsMapper
+import com.example.sendmessageskotlin.model.ChatsModel
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import kotlin.streams.toList
 
-class ChatsPresenter
-constructor(context: Context, startActivityCallBack: StartActivityCallBack){
+class ChatsPresenter {
 
-    private val USERNAME_FROM = "usernameFromChats"
-
-    private var isToday = true
-    private val context: Context
-    private lateinit var usernameFrom: String
     private val mapper = ChatsMapper()
-    private val startActivityCallBack: StartActivityCallBack
-    private val getChatsCallBack = object : CallBackHandler<ChatsEntity>{
-        override fun execute(value: ChatsEntity) {
+    private val model = ChatsModel()
 
+    fun showChats(
+        usernameFrom: String,
+        recyclerViewCallBack: RecyclerViewCallBack<List<ChatsDto>>
+    ) {
+        val getChatsCallBack = object : CallBackHandler<List<ChatsEntity>> {
+            override fun execute(value: List<ChatsEntity>) {
+                recyclerViewCallBack.get(mappingChats(value))
+            }
         }
+        model.getAllChats(getChatsCallBack, usernameFrom)
     }
 
-    init {
-        this.context = context
-        this.startActivityCallBack = startActivityCallBack
+    private fun mappingChats(value: List<ChatsEntity>): List<ChatsDto> {
+        return value
+            .toMutableList()
+            .stream()
+            .map { chatEntity -> mapping(chatEntity) }
+            .toList()
     }
 
-    fun showChats(usernameFrom: String){
-
+    private fun mapping(chatEntity: ChatsEntity): ChatsDto {
+        val zonedDateTime = ZonedDateTime.parse(
+            chatEntity.getTimeMessageToDataBase(),
+            DateFormat.getFormatFromDataBase()
+        )
+        if (zonedDateTime.toLocalDate().isEqual(LocalDate.now())) {
+            mapper.setIsToday(false)
+        } else {
+            mapper.setIsToday(true)
+        }
+        return mapper.getEntityToDto(chatEntity)
     }
 }
