d7eb39ad69f1340c51f20765e9679beac61c6f69
kirlozavr <kirill.protchenko@mail.ru>
2023-03-31 13:20:14 +1000

    Главный активити, который будет первоначально запускаться. В зависимости от состояния кнопки "запомнить меня", будет открываться та или иная activity

---
+++ app/src/main/java/com/example/sendmessageskotlin/activity/MainActivity.kt
@@ -0,0 +1,39 @@
+﻿package com.example.sendmessageskotlin.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sendmessageskotlin.common.Data
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        isSave()
+    }
+
+    private fun isSave(){
+        if(
+            Data
+                .existsPreferences(this, Data.SAVE_USERNAME)
+        ){
+            Data.putStringPreferences(
+                this,
+                Data.USERNAME,
+                Data.getStringPreferences(this, Data.SAVE_USERNAME)
+            )
+            runStartActivity(ChatsActivity::class.java)
+        } else{
+            runStartActivity(RegistrationActivity::class.java)
+        }
+    }
+
+    private fun runStartActivity(classWhere: Class<*>) {
+        val intent = Intent(
+            this@MainActivity,
+            classWhere
+        )
+        startActivity(intent)
+        finish()
+    }
+}
