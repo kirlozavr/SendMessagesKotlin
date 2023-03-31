f5e1c403d22b57a4b6aa1dc20dd1417d83734452
kirlozavr <kirill.protchenko@mail.ru>
2023-03-31 20:53:57 +1000

    Маппер для поиска пользователей

---
+++ app/src/main/java/com/example/sendmessageskotlin/mapping/SearchMapper.kt
@@ -0,0 +1,9 @@
+﻿package com.example.sendmessageskotlin.mapping
+
+import com.example.sendmessageskotlin.dto.SearchDto
+import com.example.sendmessageskotlin.entity.UserEntity
+
+class SearchMapper {
+
+    fun getEntityToDto(entity: UserEntity): SearchDto = SearchDto(entity.getUsername())
+}
